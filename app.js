796f7a64dfb78b0eca44e49b08ae2960af24c503
pankyoking <ckwlghks12+github2@gmail.com>
2022-02-17 23:03:37 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const app = express();
+const corsOptions = {
+    origin: ["http://127.0.0.1:3000"],
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send("Sorry");
+});
+app.listen(4000);
